262fcdc40ed7775f13dbe593f0ecf120cf181a62
Matt Hayes <matt@mysterycommand.com>
2016-07-26 16:29:28 -0700

    refactor: remove dummy/debugger rectangle

--- source/main.js
+++ source/main.js
@@ -1,6 +1,5 @@
 import init from './app/init';
 
 init((ctx, { ts, dts }, { w, h, hw, hh }) => {
-    ctx.fillStyle = '#f0f';
-    ctx.fillRect(hw - 50, hh - 50, 100, 100);
+    // do stuff with particles here
 });
