ef44d1b696360dfb86158b2fdfdbfeb7c7fad1e3
Matt Hayes <matt@mysterycommand.com>
2016-07-26 15:25:45 -0700

    refactor: move all the init/boilerplate into app/init, just focus on the drawing

---
+++ source/app/init.js
@@ -0,0 +1,32 @@
+import onResize from '../lib/on-resize';
+import onTick from '../lib/on-tick';
+
+export default function init(draw) {
+    const cvs = document.getElementById('js-canvas');
+    const ctx = cvs.getContext('2d');
+
+    // start these off to the default sizes for the canvas element
+    let w = 300, h = 150, hw = 150, hh = 75;
+
+    onResize(window, ({ width, height }) => {
+        cvs.width = w = width;
+        cvs.height = h = height;
+
+        hw = width / 2;
+        hh = height / 2;
+    });
+
+    onTick(({ ts, dts }) => {
+        // clean up the previous frame
+        ctx.clearRect(0, 0, w, h);
+        ctx.imageSmoothingEnabled = false;
+
+        // do work with ts or dts here
+        draw(ctx, { ts, dts }, { w, h, hw, hh });
+
+        // a super simple fps display
+        ctx.fillStyle = '#fff';
+        ctx.font = '24px monospace';
+        ctx.fillText(`${Math.round(1000 / dts)}fps`, 8, 28);
+    });
+}
--- source/main.js
+++ source/main.js
@@ -1,31 +1,6 @@
-import onResize from './lib/on-resize';
-import onTick from './lib/on-tick';
+import init from './app/init';
 
-const cvs = document.getElementById('js-canvas');
-const ctx = cvs.getContext('2d');
-
-// start these off to the default sizes for the canvas element
-let w = 300, h = 150, hw = 150, hh = 75;
-
-onResize(window, ({ width, height }) => {
-    cvs.width = w = width;
-    cvs.height = h = height;
-
-    hw = width / 2;
-    hh = height / 2;
-});
-
-onTick(({ ts, dts }) => {
-    // clean up the previous frame
-    ctx.clearRect(0, 0, w, h);
-    ctx.imageSmoothingEnabled = false;
-
-    // do work with ts or dts here
+init((ctx, { ts, dts }, { w, h, hw, hh }) => {
     ctx.fillStyle = '#f0f';
     ctx.fillRect(hw - 50, hh - 50, 100, 100);
-
-    // a super simple fps display
-    ctx.fillStyle = '#fff';
-    ctx.font = '24px monospace';
-    ctx.fillText(`${Math.round(1000 / dts)}fps`, 8, 28);
 });
