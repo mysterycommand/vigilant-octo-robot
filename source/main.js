eb8efb72313e3ebf02d5d4f41e1dcf23458e6656
Matt Hayes <matt@mysterycommand.com>
2016-07-26 17:06:24 -0700

    chore: git life

--- source/main.js
+++ source/main.js
@@ -4,16 +4,21 @@ import Particle from './lib/particle';
 const particles = [];
 
 init((ctx, { ts, dts }, { w, h, hw, hh }) => {
+    // create a new particle per frame
     particles.push(new Particle(hw, hh));
+
+    // update and render each particle
     particles.forEach(particle => {
         particle.update(dts);
         particle.render(ctx);
     });
 
+    // if we go above a limit, start removing particles
     while (particles.length > 100) {
         particles.shift();
     }
 
+    // a simple particle counter
     ctx.fillStyle = '#0ff';
     ctx.fillText(particles.length.toLocaleString(), 8, 56);
 });
