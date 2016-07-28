d1239dc097e7b4d4bd2a06870cc5f4019759ec94
Matt Hayes <matt@mysterycommand.com>
2016-07-27 18:10:18 -0700

    refactor: now that the particles rotate there's really no difference between the two

--- source/main.js
+++ source/main.js
@@ -10,15 +10,10 @@ load([
     './images/sparkle-1-1.png',
     './images/sparkle-1-2.png',
     './images/sparkle-1-1.png',
-    './images/sparkle-2-0.png',
-    './images/sparkle-2-1.png',
-    './images/sparkle-2-2.png',
-    './images/sparkle-2-1.png',
 ], images => {
     init((ctx, { ts, dts }, { w, h, hw, hh, x, y, down }) => {
         // create a new particle per frame
-        const frames = round(random()) ? images.slice(0, 4) : images.slice(4);
-        particles.push(new Particle(x, y, (round(random()) ? frames : null)));
+        particles.push(new Particle(x, y, (round(random()) ? images : null)));
 
         // update and render each particle
         particles.forEach(particle => {
